8693f55c42ff5f3df1d0405fb2c8da5cfb87df79
vogellen30 <jong0373@hz.nl>
2023-10-04 23:32:39 +0200

    Grote wijzigingen in alles, navbar en style overall

---
+++ ding.js
@@ -0,0 +1,4 @@
+let dbr = 4;
+const rest = dbr % 2;
+const IsEven = rest == 0;
+console.log("True" == true)
---
+++ resources/js/sidemenu.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+    document.getElementById("sectionindex1").style.opacity = "15%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("sidemenuknop").style.opacity = "0%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("sectionindex1").style.opacity = "100%";
+    document.body.style.backgroundColor = "#f7f7f7";
+    document.getElementById("sidemenuknop").style.opacity = "100%";
+}
