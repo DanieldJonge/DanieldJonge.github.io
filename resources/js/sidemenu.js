b50d881a33691cc168e236840000eac4f2de1dea
vogellen30 <jong0373@hz.nl>
2023-10-05 16:34:48 +0200

    its meer styling

--- resources/js/sidemenu.js
+++ resources/js/sidemenu.js
@@ -2,17 +2,17 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
     document.getElementById("main").style.marginLeft = "350px";
     document.getElementById("sectionindex1").style.opacity = "15%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.getElementById("sidemenuknop").style.opacity = "0%";
-    document.getElementById("sidemenuknop").style.transform = "rotate(90deg)"
+    document.getElementById("sidemenuknop").style.transform = "rotate(-90deg)"
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("sectionindex1").style.opacity = "100%";
-    document.body.style.backgroundColor = "#f7f7f7";
+    // document.body.style.backgroundColor = "#f7f7f7";
     document.getElementById("sidemenuknop").style.opacity = "100%";
-    document.getElementById("sidemenuknop").style.transform = "rotate(180deg)"
+    document.getElementById("sidemenuknop").style.transform = "rotate(-180deg)"
 
 }
