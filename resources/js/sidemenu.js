7cca7a0dce9ee47ea09d819e8c21e0fd91d63994
vogellen30 <jong0373@hz.nl>
2023-10-05 19:31:03 +0200

    animated image

--- resources/js/sidemenu.js
+++ resources/js/sidemenu.js
@@ -2,7 +2,6 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
     document.getElementById("main").style.marginLeft = "350px";
     document.getElementById("sectionindex1").style.opacity = "15%";
-    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.getElementById("sidemenuknop").style.opacity = "0%";
     document.getElementById("sidemenuknop").style.transform = "rotate(-90deg)"
 }
@@ -11,7 +10,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("sectionindex1").style.opacity = "100%";
-    // document.body.style.backgroundColor = "#f7f7f7";
     document.getElementById("sidemenuknop").style.opacity = "100%";
     document.getElementById("sidemenuknop").style.transform = "rotate(-180deg)"
 
